6c590185b3fc31ea7dd8cde821a41ed1500e5b06
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-07 19:00:34 +0430

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,52 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  Widget calcbutton(String btntxt, Color btncolor, Color txtcolor){
+    return Container(
+      child: ElevatedButton(
+        onPressed: (){},
+        child: Text(btntxt,style: TextStyle(
+          fontSize: 35,color: txtcolor,
+        ),
+        ),
+
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreenAccent,
-          foregroundColor: Colors.white,
-          title: Text("Calculator"),
-          centerTitle: true,
-        ),
+        backgroundColor: Colors.white,
+        // appBar: AppBar(
+        //   backgroundColor: Colors.lightGreenAccent,
+        //   foregroundColor: Colors.white,
+        //   title: Text("Calculator"),
+        //   centerTitle: true,
+        // ),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 5),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
           //   Calculator display
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(padding: EdgeInsets.all(10),
+                  child: Text("0",textAlign: TextAlign.left,style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),),)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [],
+              )
           ],),
         ),
       ),
